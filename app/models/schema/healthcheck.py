775dc2217cbcc400bcd7ae7dfd7d343603422851
pythoninja <pythoninja@users.noreply.github.com>
2021-08-01 20:15:11 +0300

    feat: add version number to /healthcheck endpoint

--- app/models/schema/healthcheck.py
+++ app/models/schema/healthcheck.py
@@ -3,4 +3,5 @@ from pydantic import BaseModel
 
 
 class HealthcheckSchema(BaseModel):
+    version: str
     status: str
--- app/routers/healthcheck.py
+++ app/routers/healthcheck.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python
 from fastapi import APIRouter
 
+from app import __version__
 from app.models.schema.healthcheck import HealthcheckSchema
+
 
 router = APIRouter()
 
 
 @router.get('/healthcheck')
 async def get_status():
-    data = {'status': 'UP'}
+    data = {
+        'version': __version__,
+        'status': 'UP'
+    }
 
     return HealthcheckSchema(**data)
