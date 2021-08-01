37a69c1c049994cf5d3a774b0f7bc9babaa80d02
pythoninja <pythoninja@users.noreply.github.com>
2021-08-01 20:15:49 +0300

    test: add tests to version number in /healthcheck endpoint

--- tests/test_healthcheck_router.py
+++ tests/test_healthcheck_router.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python
 from requests import Response
 
+from app import __version__
+
 
 def test_healthcheck_router(client):
     with client:
         response: Response = client.get('/healthcheck')
 
     response_structure = {
-        'status': 'UP'
+        'status': 'UP',
+        'version': __version__
     }
 
     assert response.status_code == 200
     assert response_structure.keys() == response.json().keys()
     assert response_structure == response.json()
+    assert response.json()['version'] == __version__
