1284b334b3c0eb6ef8ad1046290eb6f5b9d3a6d5
pythoninja <pythoninja@users.noreply.github.com>
2022-05-31 20:52:47 +0300

    refactor(event): move send_event function to class
    Closes #253

---
+++ app/helpers/event.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+from app.db import database
+from app.enums import EventType
+from app.models.db.event import event_table
+
+
+class Event:
+    @staticmethod
+    async def send_event(event_type: EventType, language: str = 'en'):
+        query = event_table.insert().values(
+            event_type=event_type,
+            language=language
+        )
+        return await database.execute(query=query)
