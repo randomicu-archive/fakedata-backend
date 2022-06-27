c901294cbb237a07a54976eee661cfdf7ffba452
pythoninja <pythoninja@users.noreply.github.com>
2022-06-27 08:57:44 +0300

    refactor(uuid): move logic from router

--- app/responses/uuid.py
+++ app/responses/uuid.py
@@ -1,17 +1,29 @@
 #!/usr/bin/env python
 import uuid
 
+from fastapi import HTTPException
+from starlette import status
+
 from app.models.schema.uuid import NestedUuidSchema
 from app.models.schema.uuid import UuidSchema
 from app.responses.response import Response
 
 
 class UuidResponse(Response):
-    def __init__(self, uppercase: bool, version: int):
+    def __init__(self, count: int, uppercase: bool, version: int):
+        self.count = count
         self.uppercase = uppercase
         self.version = version
 
-    def generate(self) -> UuidSchema:
+    def generate(self):
+        if self.version != 4:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                                detail='Unsupported uuid version. '
+                                       'Use version `4` as a query param')
+
+        return [self._generate_schema() for _ in range(self.count)]
+
+    def _generate_schema(self):
         random_uuid: str = str(uuid.uuid4())
         _uuid: str = random_uuid if not self.uppercase else random_uuid.upper()
         nested_uuid_schema: NestedUuidSchema = NestedUuidSchema(uuid=_uuid, version=self.version)
